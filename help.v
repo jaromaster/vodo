5f7d360ed68e9e648d4a2ac80d542a2715967c5a
Jaromir Blutaumüller <jaromirblu@gmail.com>
2022-12-15 20:54:03 +0100

    updated print_help

--- help.v
+++ help.v
@@ -2,12 +2,14 @@
 // print help
 pub fn print_help() {
     println("SYNTAX: vodo [CMD] [VALUE]")
+	
 	println("Commands:")
 	println("\tinit ... init vodo")
 	println("\thelp ... print this help")
 	println("\treset ... reset vodo")
 	println("\tadd ... add new task")
-	println("\tdel ... del task")
+	println("\tdel ... delete task")
 	println("\tlist ... list all tasks")
 
+	// TODO examples, command usage
 }
