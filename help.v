09ff4063a555c145561eedbf9058c04b4558d3bb
Jaromir Blutaumüller <jaromirblu@gmail.com>
2022-12-22 21:29:15 +0100

    updated help

--- help.v
+++ help.v
@@ -10,7 +10,7 @@ pub fn print_help() {
 	println("\tinit ... init and setup vodo")
 	println("\thelp ... print this help")
 	println("\treset ... reset vodo (remove all data)")
-	println("\tadd ... add new task")
+	println("\tadd ... add new task (with optional deadline)")
 	println("\tdel ... delete task with id")
 	println("\tsearch ... search for specific task")
 	println("\tlist ... list all tasks")
@@ -20,7 +20,7 @@ pub fn print_help() {
 	println("\tinit ... 'vodo init'")
 	println("\thelp ... 'vodo help'")
 	println("\treset ... 'vodo reset'")
-	println("\tadd ... 'vodo add \"some-task\"'") // TODO time support and format: "YYYY-MM-DD HH:mm:ss"
+	println("\tadd ... 'vodo add \"some-task\" \"YYYY-MM-DD HH:mm:ss\"' or 'vodo add \"some-task\" -") // TODO time support and format: "YYYY-MM-DD HH:mm:ss"
 	println("\tdel ... 'vodo del ID'")
 	println("\tsearch ... 'vodo search \"some-task\"'")
 	println("\tlist ... 'vodo list'")
